b60ca416157b5750a96271e8cf8c93ab1624d8cc
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-04-14 18:02:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Menu/>
      
          <Routes>
-            <Route exact path="/" element={ <Home/> } />
+            <Route path="/" element={ <Home/> } />
             <Route path="/contact" element={ <Contact/> } />
          </Routes>
 
