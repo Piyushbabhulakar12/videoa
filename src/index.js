479316a37bab58b33bc5c48fe91a502b81cbc35c
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-04-14 18:19:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename ="/videoa">
+  <BrowserRouter basename={window.location.pathname || ''}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
