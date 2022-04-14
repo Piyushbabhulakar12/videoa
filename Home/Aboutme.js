7207636d4976dea95b4d0d71e905d979223d49da
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-04-14 18:55:28 +0530

    Add files via upload

---
+++ Home/Aboutme.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+function Aboutme() {
+    return (
+      <div>
+        
+
+        <div class="container section_margin_top">
+            <h2 class="heading_text_about">About Me</h2>
+        </div>
+
+        <div class="container mt-5">
+            <div class="box_padd">
+                
+                <div class="row">
+                  <div class="col-md-6 box_padd">
+                   
+                   <h3>Demo Text</h3>
+                   <p class="about_para_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
+
+                  </div>
+                  <div class="col-md-6 box_padd">
+                  <h3>Demo Text</h3>
+                   <p class="about_para_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 box_padd">
+                   
+                   <h3>Demo Text</h3>
+                   <p class="about_para_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
+
+                  </div>
+                  <div class="col-md-6 box_padd">
+                  <h3>Demo Text</h3>
+                   <p class="about_para_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
+                  </div>
+                </div>
+
+            </div>
+        </div>
+          
+           
+      </div>
+    );
+  }
+  
+  export default Aboutme;
