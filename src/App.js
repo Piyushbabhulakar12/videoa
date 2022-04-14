4dc386ec4b6ceb638421e28049acbe4d4acb865b
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-04-14 18:07:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Router, Routes, Route } from "react-router-dom"
 import './App.css';
 import Menu from './Menu.js';
 import Home from './Home.js';
@@ -9,11 +9,12 @@ function App() {
   return (
     <div>
           <Menu/>
-     
+      <Router basename="/videoa">
          <Routes>
             <Route path="/" exact element={ <Home/> } />
             <Route path="/contact" element={ <Contact/> } />
          </Routes>
+      </Router>
 
     </div>
   );
